ece83c8586dbe93001223eb6100ed3cf7dca3bbf
renelopez <rene.lopez.cano@gmail.com>
2018-10-17 00:45:15 -0500

    rtrtrt

--- client/pages/index.js
+++ client/pages/index.js
@@ -23,81 +23,20 @@ export default  () => (
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogActions from '@material-ui/core/DialogActions';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import Link from 'next/link';
-import posts from '../data/posts';
 
-
-const styles = theme => ({
-  root: {
-    textAlign: 'center',
-    paddingTop: theme.spacing.unit * 20,
-  },
-});
+import LoginComponent from '../auth/Login';
 
 class Index extends React.Component {
-  state = {
-    open: false,
-  };
 
-  handleClose = () => {
-    this.setState({
-      open: false,
-    });
-  };
-
-  handleClick = () => {
-    this.setState({
-      open: true,
-    });
-  };
+  constructor(props){
+    super(props);
+  }
 
   render() {
-    const { classes } = this.props;
-    const { open } = this.state;
+
 
     return (
-
-      <div className={classes.root}>
-        <ul>
-          {posts.map((post,index) => (
-            <li key={index}>
-              <Link as={`/post/${index}`} href={{pathname: '/about', query: {id: index}}}>
-                <a>{post.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <Dialog open={open} onClose={this.handleClose}>
-          <DialogTitle>Super Secret Password</DialogTitle>
-          <DialogContent>
-            <DialogContentText>1-2-3-4-5</DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button color="primary" onClick={this.handleClose}>
-              OK
-            </Button>
-          </DialogActions>
-        </Dialog>
-        <Typography variant="h4" gutterBottom>
-          Material-UI
-        </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          example project
-        </Typography>
-        <Typography gutterBottom>
-        </Typography>
-        <Button variant="contained" color="secondary" onClick={this.handleClick}>
-          Super Secret Password
-        </Button>
-      </div>
+      <LoginComponent/>
     );
   }
 }
@@ -106,4 +45,4 @@ Index.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(Index);
+export default Index
