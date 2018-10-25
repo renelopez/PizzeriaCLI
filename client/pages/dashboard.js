c2da239e6660215e2c361c9fbb8155cc5fd9e88e
renelopez <rene.lopez.cano@gmail.com>
2018-10-24 23:42:03 -0500

    Added grid

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
+import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme =>({
+  demo:{
+    padding: theme.spacing.unit * 2,
+    color: theme.palette.text.secondary,
+    minHeight:'100vh'
+  },
+  cardItem:{
+    padding:'30vh'
+  },
+  visionLayout:{
+    height: '100%'
+  },
+  root:{
+    height:'100%'
+  }
   // dashboardContainer:{
   //   display:'flex',
   //   alignItems:'center',
@@ -71,30 +86,29 @@ class Dashboard extends React.Component {
   render(){
     const { classes } = this.props;
     return (
-      <Grid container spacing={16} alignItems="stretch">
-        <Grid item xs={12}>
-          <Card>
-          <Grid container spacing={16} direction="row" justify="center" alignItems="center">
-            <Grid key={0} item>
-              <Card>
-                <Typography  variant="h5" gutterBottom>Title 1</Typography>
-              </Card>
-            </Grid>
-            <Grid key={1} item>
-              <Card>
-                <Typography  variant="h5" gutterBottom>Title 2</Typography>
-              </Card>
-            </Grid>
-            <Grid key={2} item>
-              <Card>
-                <Typography  variant="h5" gutterBottom>Title 3</Typography>
-              </Card>
-            </Grid>
-          </Grid>
-          </Card>
-        </Grid>
+      <Grid container justify="space-between" alignItems="center" className={classes.visionLayout} spacing={16}>
+        {/*<Grid item xs={12}>*/}
+          {/*<Paper>*/}
+            {/*<Grid container direction="row" spacing={16} justify="center" alignItems="center">*/}
+              <Grid key={0} item xs={4}>
+                <Card>
+                  <Typography  variant="h5" gutterBottom>Title 1</Typography>
+                </Card>
+              </Grid>
+              <Grid key={1} item  xs={4} >
+                <Card>
+                  <Typography  variant="h5" gutterBottom>Title 2</Typography>
+                </Card>
+              </Grid>
+              <Grid key={2} item  xs={4}>
+                <Card>
+                  <Typography  variant="h5" gutterBottom>Title 3</Typography>
+                </Card>
+              </Grid>
+            {/*</Grid>*/}
+          {/*</Paper>*/}
+        {/*</Grid>*/}
       </Grid>
-
 
     )
   }
